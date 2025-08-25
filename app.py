3d140790f885a4bc46e9d60459e084e45e312af4
Rishiiitha <rishithavl15@gmail.com>
2025-08-25 15:16:50 +0530

    modified code

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ model, scaler = pickle.load(open("trained_model.sav", "rb"))
 st.title("Credit Card Availability Prediction")
 st.write("Enter user details below:")
 
-Age = st.number_input("Age (years):", min_value=1, max_value=100, value=30)
-Experience = st.number_input("Experience (years):", min_value=0, max_value=50, value=5)
-Income = st.number_input("Income (LPA):", min_value=0, max_value=250, value=50)
-Family = st.number_input("Family (count):", min_value=1, max_value=10, value=2)
-CCAvg = st.number_input("Average Credit Card Spending:", min_value=0.0, max_value=10.0, value=1.0)
-Education = st.selectbox("Education Level:", [1, 2, 3])  # 1=Undergrad, 2=Graduate, 3=Advanced/Professional
-Mortgage = st.number_input("Mortgage:", min_value=0, max_value=650, value=0)
-Personal_Loan = st.selectbox("Personal Loan:", [0, 1])  # 0=No, 1=Yes
-Securities_Account = st.selectbox("Securities Account:", [0, 1])
-CD_Account = st.selectbox("CD Account:", [0, 1])
-Online = st.selectbox("Online Banking:", [0, 1])
+Age = st.number_input("Age (years)[1 to 100]:", min_value=1, max_value=100, value=30)
+Experience = st.number_input("Experience (years)[0 to 100]:", min_value=0, max_value=50, value=5)
+Income = st.number_input("Income (LPA)[0 to 250]:", min_value=0, max_value=250, value=50)
+Family = st.number_input("Family (count)[1 to 4]:", min_value=1, max_value=4, value=2)
+CCAvg = st.number_input("Average Credit Card Spending[0 to 10]:", min_value=0.0, max_value=10.0, value=1.0)
+Education = st.selectbox("Education Level:[1 or 2 or 3]", [1, 2, 3])  # 1=Undergrad, 2=Graduate, 3=Advanced/Professional
+Mortgage = st.number_input("Mortgage[0 to 650]:", min_value=0, max_value=650, value=0)
+Personal_Loan = st.selectbox("Personal Loan[0 or 1]:", [0, 1])  # 0=No, 1=Yes
+Securities_Account = st.selectbox("Securities Account[0 or 1]:", [0, 1])
+CD_Account = st.selectbox("CD Account:[0 or 1]", [0, 1])
+Online = st.selectbox("Online Banking[0 or 1]:", [0, 1])
 
 features = np.array([[Age, Experience, Income, Family, CCAvg, Education,
                       Mortgage, Personal_Loan, Securities_Account, CD_Account, Online]])
